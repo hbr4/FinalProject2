159db39c3e7ba32aba1b0d33d8f12694803a191c
Hiren Rana <hbr4@njit.edu>
2020-05-11 20:16:39 -0400

    Updated app.py file for Tree

--- app/app.py
+++ app/app.py
@@ -12,108 +12,104 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'zillowesData'
+app.config['MYSQL_DATABASE_DB'] = 'treesData'
 mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Zillow Project'}
+    user = {'username': 'Tree Project'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblZillowImport')
+    cursor.execute('SELECT * FROM tblTreeImport')
     result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, zillowes=result)
+    return render_template('index.html', title='Home', user=user, trees=result)
 
 
-@app.route('/view/<int:zillow_id>', methods=['GET'])
-def record_view(zillow_id):
+@app.route('/view/<int:tree_id>', methods=['GET'])
+def record_view(tree_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblZillowImport WHERE id=%s', zillow_id)
+    cursor.execute('SELECT * FROM tblTreeImport WHERE id=%s', tree_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', zillow=result[0])
+    return render_template('view.html', title='View Form', tree=result[0])
 
 
-@app.route('/edit/<int:zillow_id>', methods=['GET'])
-def form_edit_get(zillow_id):
+@app.route('/edit/<int:tree_id>', methods=['GET'])
+def form_edit_get(tree_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblZillowImport WHERE id=%s', zillow_id)
+    cursor.execute('SELECT * FROM tblTreeImport WHERE id=%s', tree_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', zillow=result[0])
+    return render_template('edit.html', title='Edit Form', tree=result[0])
 
 
-@app.route('/edit/<int:zillow_id>', methods=['POST'])
-def form_update_post(zillow_id):
+@app.route('/edit/<int:tree_id>', methods=['POST'])
+def form_update_post(tree_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Living_Space_sq_ft'), request.form.get('Beds'), request.form.get('Baths'),
-                 request.form.get('Zip'), request.form.get('Year'),
-                 request.form.get('List_Price'), zillow_id)
-    sql_update_query = """UPDATE tblZillowImport t SET t.Living_Space_sq_ft = %s, t.Beds = %s, t.Baths = %s, t.Zip = 
-    %s, t.Year = %s, t.List_Price = %s WHERE t.id = %s """
+    inputData = (request.form.get('Girth_in'), request.form.get('Height_ft'), request.form.get('Volume_ft3'),
+                  tree_id)
+    sql_update_query = """UPDATE tblTreeImport t SET t.Girth_in = %s, t.Height_ft = %s, t.Volume_ft3 = %s, WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/zillowes/new', methods=['GET'])
+@app.route('/trees/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New zillow Form')
+    return render_template('new.html', title='New tree Form')
 
 
-@app.route('/zillowes/new', methods=['POST'])
+@app.route('/trees/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Living_Space_sq_ft'), request.form.get('Beds'), request.form.get('Baths'),
-                 request.form.get('Zip'), request.form.get('Year'),
-                 request.form.get('List_Price'))
-    sql_insert_query = """INSERT INTO tblZillowImport (Living_Space_sq_ft,Beds,Baths,Zip,Year,List_Price) VALUES (%s,%s,%s,%s,%s,%s) """
+    inputData = (request.form.get('Girth_in'), request.form.get('Height_ft'), request.form.get('Volume_ft3'))
+    sql_insert_query = """INSERT INTO tblTreeImport (Girth_in,Height_ft,Volume_ft3) VALUES (%s,%s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/delete/<int:zillow_id>', methods=['POST'])
-def form_delete_post(zillow_id):
+@app.route('/delete/<int:tree_id>', methods=['POST'])
+def form_delete_post(tree_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tblZillowImport WHERE id = %s """
-    cursor.execute(sql_delete_query, zillow_id)
+    sql_delete_query = """DELETE FROM tblTreeImport WHERE id = %s """
+    cursor.execute(sql_delete_query, tree_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/zillowes', methods=['GET'])
+@app.route('/api/v1/trees', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblZillowImport')
+    cursor.execute('SELECT * FROM tblTreeImport')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/zillowes/<int:zillow_id>', methods=['GET'])
-def api_retrieve(zillow_id) -> str:
+@app.route('/api/v1/trees/<int:tree_id>', methods=['GET'])
+def api_retrieve(tree_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblZillowImport WHERE id=%s', zillow_id)
+    cursor.execute('SELECT * FROM tblTreeImport WHERE id=%s', tree_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/zillowes/', methods=['POST'])
+@app.route('/api/v1/trees/', methods=['POST'])
 def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/zillowes/<int:zillow_id>', methods=['PUT'])
-def api_edit(zillow_id) -> str:
+@app.route('/api/v1/trees/<int:tree_id>', methods=['PUT'])
+def api_edit(tree_id) -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/zillowes/<int:zillow_id>', methods=['DELETE'])
-def api_delete(zillow_id) -> str:
+@app.route('/api/trees/<int:tree_id>', methods=['DELETE'])
+def api_delete(tree_id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
